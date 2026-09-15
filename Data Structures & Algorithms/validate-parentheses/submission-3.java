aa0624f1e4389662524891a6b2f60dbff946d2be
Vishal Shah <vishal205014@gmail.com>
2026-09-15 18:50:51 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(']', '[');
+        map.put('}', '{');
+        map.put(')', '(');
+
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                if (!stk.isEmpty() && stk.peek() == map.get(c)) stk.pop();
+                else return false;
+            } else {
+                stk.push(c);
+            }
+        }
+
+        return stk.isEmpty();
+    }
+}
